300daf2c4e0756a8664e564d38869f50ba9015de
CosmiaByTheRiver <107163101+CosmiaByTheRiver@users.noreply.github.com>
2023-07-19 20:03:32 -0700

    Write HTML boilerplate & create div inside 'container' w/ js

---
+++ etch.js
@@ -0,0 +1,12 @@
+                // CONTAINER & GRID \\
+// ------------------------------------------------ \\
+
+const container = document.getElementById('container');
+
+const grid = document.createElement('div');
+
+grid.id = 'grid';
+
+container.appendChild(grid);
+
+// ------------------------------------------------ \\
