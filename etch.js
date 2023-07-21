6c85b4c7f8fa0505fb238e033a454a01ca6cb38d
CosmiaByTheRiver <107163101+CosmiaByTheRiver@users.noreply.github.com>
2023-07-20 21:56:12 -0700

    Write function to make desired amount of divs

--- etch.js
+++ etch.js
@@ -1,90 +1,15 @@
-                // CONTAINER\\
-// ------------------------------------------------ \\
 
 const container = document.getElementById('container');
 
-// --------------------------------------------------------------- \\
 
+function makeGrid(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
 
-                // GRID DIVs \\
-// ------------------------------------------------ \\
+    for (i = 0; i < (rows * cols); i++) {
+        let generate = document.createElement('div');
+        container.appendChild(generate).className = 'grid-unit';
+    }
+}
 
-const grid1 = document.createElement('div');
-grid1.id = "grid1";
-grid1.classList.add('grids')
-container.appendChild(grid1);
-
-const grid2 = document.createElement('div');
-grid2.id = "grid2";
-grid2.classList.add('grids')
-container.appendChild(grid2);
-
-const grid3 = document.createElement('div');
-grid3.id = "grid3";
-grid3.classList.add('grids')
-container.appendChild(grid3);
-
-const grid4 = document.createElement('div');
-grid4.classList.add('grids')
-grid4.id = "grid4";
-container.appendChild(grid4);
-
-const grid5 = document.createElement('div');
-grid5.classList.add('grids')
-grid5.id = "grid5";
-container.appendChild(grid5);
-
-const grid6 = document.createElement('div');
-grid6.classList.add('grids')
-grid6.id = "grid6";
-container.appendChild(grid6);
-
-const grid7 = document.createElement('div');
-grid7.classList.add('grids')
-grid7.id = "grid7";
-container.appendChild(grid7);
-
-const grid8 = document.createElement('div');
-grid8.classList.add('grids')
-grid8.id = "grid8";
-container.appendChild(grid8);
-
-const grid9 = document.createElement('div');
-grid9.classList.add('grids')
-grid9.id = "grid9";
-container.appendChild(grid9);
-
-const grid10 = document.createElement('div');
-grid10.classList.add('grids')
-grid10.id = "grid10";
-container.appendChild(grid10);
-
-const grid11 = document.createElement('div');
-grid11.classList.add('grids')
-grid11.id = "grid11";
-container.appendChild(grid11);
-
-const grid12 = document.createElement('div');
-grid12.classList.add('grids')
-grid12.id = "grid12";
-container.appendChild(grid12);
-
-const grid13 = document.createElement('div');
-grid13.classList.add('grids')
-grid13.id = "grid13";
-container.appendChild(grid13);
-
-const grid14 = document.createElement('div');
-grid14.classList.add('grids')
-grid14.id = "grid14";
-container.appendChild(grid14);
-
-const grid15 = document.createElement('div');
-grid15.classList.add('grids')
-grid15.id = "grid15";
-container.appendChild(grid15);
-
-const grid16 = document.createElement('div');
-grid16.classList.add('grids')
-grid16.id = "grid16";
-container.appendChild(grid16);
+makeGrid(16, 16);
