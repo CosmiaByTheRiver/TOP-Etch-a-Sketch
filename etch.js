c82cd76b1cb4ddfde3d2ce516708fec804884875
CosmiaByTheRiver <107163101+CosmiaByTheRiver@users.noreply.github.com>
2023-07-25 20:56:38 -0700

    Simplify code to re-make grid in changeGrid function

--- etch.js
+++ etch.js
@@ -1,19 +1,19 @@
-
 const container = document.getElementById('container');
+let cellColor;
 
 
                 // MAKEGRID FUNCTION \\
 // ----------------------------------------------- \\
-let generate;
 
 function makeGrid(rows = 16, cols = 16) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
 
     for (i = 0; i < (rows * cols); i++) {
-        generate = document.createElement('div');
+        let generate = document.createElement('div');
         container.appendChild(generate).className = 'grid-unit';
     }
+    cellEvent();
 }
 
 makeGrid();
@@ -22,16 +22,20 @@ makeGrid();
 
 
 
-                // CHANGE CELL COLOR - MOUSEOVER \\
+
+
+          // CHANGE CELL COLOR - MOUSEOVER \\
 // ----------------------------------------------- \\
 
-const cellColor = document.querySelectorAll('.grid-unit');
+function cellEvent() {
+    cellColor = document.querySelectorAll('.grid-unit');
 
 cellColor.forEach((e) => {
     e.addEventListener('mouseover', () => {
         e.style.backgroundColor = 'pink';
-    })
-})
+        });
+    });
+};
 
 // -------------------------------------------------------------------- \\
 
@@ -49,17 +53,16 @@ let dimInput;
         dimInput = prompt("Enter desired grid dimension");
 
         function changeGrid() {
-            if (dimInput <= 100) {
-            // container.remove('style');
-            container.setAttribute('style', `--grid-rows: ${dimInput}; --grid-cols:
-            ${dimInput}`);
 
+            if (dimInput <= 100) {
+                cellColor.forEach((e) => {
+                    e.remove();
+                });
+
+                for (i = 0; i < (dimInput * dimInput); i++) {
+                    makeGrid(dimInput, dimInput);
+                }
             };
         };
-
         changeGrid();
     });
-
-
-
-// -------------------------------------------------------------------- \\
