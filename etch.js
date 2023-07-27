df0de27dcca01e73c439bcd8b6cf1b01f62a0170
CosmiaByTheRiver <107163101+CosmiaByTheRiver@users.noreply.github.com>
2023-07-26 18:04:58 -0700

    Write functions that randomize cell colors on mouseover

--- etch.js
+++ etch.js
@@ -13,7 +13,7 @@ function makeGrid(rows = 16, cols = 16) {
         let generate = document.createElement('div');
         container.appendChild(generate).className = 'grid-unit';
     }
-    cellEvent();
+    changeColors();
 }
 
 makeGrid();
@@ -27,15 +27,30 @@ makeGrid();
           // CHANGE CELL COLOR - MOUSEOVER \\
 // ----------------------------------------------- \\
 
-function cellEvent() {
-    cellColor = document.querySelectorAll('.grid-unit');
+function numGenerator(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
 
-cellColor.forEach((e) => {
-    e.addEventListener('mouseover', () => {
-        e.style.backgroundColor = 'pink';
-        });
-    });
-};
+
+function randomRgbColor() {
+    let r = numGenerator(255);
+    let g = numGenerator(255);
+    let b = numGenerator(255);
+    return [r, g, b];
+}
+
+
+function changeColors() {
+    cellColor = document.querySelectorAll('.grid-unit');
+    cellColor.forEach((e) => {
+        e.addEventListener('mouseover', () => {
+            let rgb = randomRgbColor();
+            e.style.backgroundColor = 'rgb(' + rgb.toString() + ')';
+        })
+    })
+}
+changeColors();
+
 
 // -------------------------------------------------------------------- \\
 
